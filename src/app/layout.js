199bccedac096d5f4667fef62d24a8591e26bae4
Sandeep Chandna <grow@thetous.com>
2026-05-27 12:32:15 +0530

    feat: install GA4 base script in layout and env example

--- src/app/layout.js
+++ src/app/layout.js
@@ -64,6 +64,24 @@ export default function RootLayout({ children }) {
           </Script>
         )}
 
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+          <>
+            <Script
+              id="ga4-loader"
+              strategy="afterInteractive"
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+            />
+            <Script id="ga4-init" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+              `}
+            </Script>
+          </>
+        )}
+
       </head>
       <body
         className={`antialiased ${lato.variable} ${poppins.variable}`}
